564c9495634f50217ad7940fb9238eb97e9aa725
Gabriel Antunes Tosi <65515519+xTosi@users.noreply.github.com>
2022-12-05 09:26:27 -0300

    ajustes no ranking

--- _JSfiles/ranking.js
+++ _JSfiles/ranking.js
@@ -1,109 +1,29 @@
-function eventos() {
-
-    const lista = [
-        {
-            'doce': '../images/ranking_img1.png',
-            'nome': 'João C.',
-            'cozimento': '10min',
-            'porcoes': '4x4',
-            'avaliacao': '100 pts'
-        },
-        {
-            'doce': ' ../images/ranking_img1.png',
-            'nome': 'Lucas .',
-            'cozimento': '15min',
-            'porcoes': '4x4',
-            'avaliacao': '120 pts'
-        },
-        {
-            'doce': ' ../images/ranking_img1.png',
-            'nome': 'Pedro L.',
-            'cozimento': '20min',
-            'porcoes': '8x8',
-            'avaliacao': '200 pts'
-        },
-        {
-            'doce': '../images/ranking_img1.png',
-            'nome': 'Patricia D.',
-            'cozimento': '12min',
-            'porcoes': '4x4',
-            'avaliacao': '90 pts'
-        },
-        {
-            'doce': '../images/ranking_img1.png',
-            'nome': 'Sabrina C.',
-            'cozimento': '12min',
-            'porcoes': '4x4',
-            'avaliacao': '100 pts'
-        },
-        {
-            'doce': '../images/ranking_img1.png',
-            'nome': 'Fábio F.',
-            'cozimento': '11min',
-            'porcoes': '4x4',
-            'avaliacao': '100 pts'
-        },
-        {
-            'doce': '../images/ranking_img1.png',
-            'nome': 'Gabriel F.',
-            'cozimento': '10min',
-            'porcoes': '4x4',
-            'avaliacao': '130 pts'
-        },
-        {
-            'doce': '../images/ranking_img1.png',
-            'nome': 'Pedro G.',
-            'cozimento': '10min',
-            'porcoes': '4x4',
-            'avaliacao': '130 pts'
-        },
-        {
-            'doce': '../images/ranking_img1.png',
-            'nome': 'Rafael F.',
-            'cozimento': '10min',
-            'porcoes': '4x4',
-            'avaliacao': '130 pts'
-        },
-        {
-            'doce': '../images/ranking_img1.png',
-            'nome': 'Gabriel F.',
-            'cozimento': '10min',
-            'porcoes': '4x4',
-            'avaliacao': '130 pts'
-        }
-    ];
-
-    function listaJogadores(jogador) {
-        return `
-        <li>
-            <div class="coluna1"><img src="${jogador.doce}" alt="candy"><p>${jogador.nome}</p>
-                <div class="info_player">
-                    <div class="linha_info"><div class="tipo">Nome:</div><div class="dado">João</div></div>
-                    <div class="linha_info"><div class="tipo">Dimensão:</div><div class="dado">4x4</div></div>
-                    <div class="linha_info"><div class="tipo">Modalidade:</div><div class="dado">Contra Tempo</div></div>
-                    <div class="linha_info"><div class="tipo">Duração:</div><div class="dado">10:00</div><div class="tipo">Resultado:</div><div class="dado">Vitória</div></div>
-                    <div class="linha_info"><div class="tipo">Data/Hora:</div><div class="dado">11/10/2022 - 12:05</div></div>
-                </div>
-            </div>
-            <div class="coluna2">
-                <div class="campo"><img src="../images/ranking_img2.png" alt="time"><p class="tipo">Cozimento:</p><p class="dado">${jogador.cozimento}</p></div>
-                <div class="campo, campo2"><img src="../images/ranking_img3.png" alt="cutlery"><p class="tipo">Porções:</p><p class="dado">${jogador.porcoes}</p></div>
-                <div class="campo"><img src="../images/ranking_img4.png" alt="rating"><p class="tipo">Avaliação:</p><p class="dado">${jogador.avaliacao}</p></div>
-            </div>
-        </li>
-        <li class="linha"></li>
-        `
-    }
-
-    document.getElementById('tabela').innerHTML = `
+function listaJogadores(partida) {
+    console.log(partida)
+    return `
     <li>
-        <div class="coluna1"><p>Ingredientes</p></div>
-        <div class="coluna2"><p>Modo de preparo</p></div>
+        <div class="coluna1"><img src="../images/ranking_img1.png" alt="candy"><p>${partida.username}</p>
+            <div class="info_player">
+                <div class="linha_info"><div class="tipo">Nome:</div><div class="dado">${partida.nome_completo}</div></div>
+                <div class="linha_info"><div class="tipo">Dimensão:</div><div class="dado">${partida.dimensao}</div></div>
+                <div class="linha_info"><div class="tipo">Modalidade:</div><div class="dado">${partida.modalidade}</div></div>
+                <div class="linha_info"><div class="tipo">Duração:</div><div class="dado">${partida.tempo_partida}</div><div class="tipo">Resultado:</div><div class="dado">${partida.resultado}</div></div>
+                <div class="linha_info"><div class="tipo">Data/Hora:</div><div class="dado">${partida.horario_partida}</div></div>
+            </div>
+        </div>
+        <div class="coluna2">
+            <div class="campo"><img src="../images/ranking_img2.png" alt="time"><p class="tipo">Cozimento:</p><p class="dado">${partida.tempo_partida}</p></div>
+            <div class="campo, campo2"><img src="../images/ranking_img3.png" alt="cutlery"><p class="tipo">Porções:</p><p class="dado">${partida.dimensao}</p></div>
+            <div class="campo"><img src="../images/ranking_img4.png" alt="rating"><p class="tipo">Avaliação:</p><p class="dado">${partida.pontos}</p></div>
+        </div>
     </li>
-    ${lista.map(listaJogadores).join('')}
-    `;
+    <li class="linha"></li>
+    `
+}
 
+function eventos(dadosRanking) {
     nomeJogadores = document.getElementsByClassName('coluna1');
+    console.log(nomeJogadores)
     
     for (i = 1; i < nomeJogadores.length; i++) {
         nomeJogadores[i].addEventListener('mouseover', function(){
